2a7bd06ffc1837af6bf85cadf3ca85193ffdd965
Simran <mdrsimran@gmail.com>
2020-11-03 09:03:06 -0600

    commit tree 8ac4a61458f56b87eb00e75ada9b040a8ec25edf

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence?()
+    # binding.pry
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    array=[]
+    count= 0
+# binding.pry
+array=self.split(/[.?!]/).grep(/\S/)
+count = array.length
+return count
   end
 end
